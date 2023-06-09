0fa2bd123984aa35cd5a4df134315b8fbfdc35fa
zhaochangchang <zhaochangchang@shenshiads.com>
2023-06-09 18:34:01 +0800

    测试激励视频

---
+++ ads.js
@@ -0,0 +1,10 @@
+var adReward = null
+
+function rewardedVideoInit(adpid, callbackData) {
+  adReward = plus.ad.createRewardedVideoAd({
+    adpid: adpid,
+    urlCallback: callbackData
+  });
+}
+
+export default {adReward}
