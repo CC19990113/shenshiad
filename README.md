646da14b773f2e15e8416b7f444d7745bfaf91a6
zhaochangchang <zhaochangchang@shenshiads.com>
2023-06-12 18:51:48 +0800

    update

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# test
+# H5对接文档
+
+## 项目准备
+
+在H5项目中引入uni-webview
+1. 原生HTML 引入JS文件[文件地址](https://fpvideo.shenshiads.com/h5tovue/index.js)
+
+```js
+<script src="https://fpvideo.shenshiads.com/h5tovue/index.js"></script>
+```
+2. 
--- ads.js
+++ ads.js
@@ -1,8 +1,24 @@
 export var adReward = null
 
-export function rewardedVideoInit(adpid, callbackData) {
+export function rewardedVideoInit(adpid, callbackData = {}) {
   adReward = plus.ad.createRewardedVideoAd({
     adpid: adpid,
     urlCallback: callbackData
+  });
+}
+
+export var adInter = null
+
+export function interstitialInit(adpid) {
+  adInter = plus.ad.createInterstitialAd({
+    adpid: adpid
+  });
+}
+
+export var adFull = null
+
+export function fullScreenInit(adpid) {
+  adFull = plus.ad.createFullScreenVideoAd({
+    adpid: adpid
   });
 }
